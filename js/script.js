7813496b966856a1fae5ae79b2467f402a958cc8
PauloMeneghini <64617184+PauloMeneghini@users.noreply.github.com>
2022-09-27 22:05:53 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const cbTermo = document.getElementById("cbTermo");
 const btnTermo = document.getElementById("btnTermo");
+const linkTermo = document.getElementById("linkTermo");
 
 function verifyCheckBox(){
     if (cbTermo.checked == true){
         btnTermo.className = "active";
+        linkTermo.className = "linkTermoActive";
     } else {
         btnTermo.className = "btnTermo";
+        linkTermo.className = "linkTermo";
     }
 }
