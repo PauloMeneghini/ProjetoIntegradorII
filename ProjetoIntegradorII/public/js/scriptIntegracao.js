3aab29bd98430ae7f5725bc8fa71ca0c89715d5c
Paulo Henrique Meneghini <paulo.meneghini@outlook.com>
2022-11-02 18:09:38 -0300

    Adicionado input celular no cadastro

--- ProjetoIntegradorII/public/js/scriptIntegracao.js
+++ ProjetoIntegradorII/public/js/scriptIntegracao.js
@@ -57,50 +57,88 @@ function cadastrarUsuario() {
     const nomeUsuario = document.getElementById("nomeUsuario").value;
     const emailCadastro = document.getElementById("emailCadastro").value;
     const senhaCadastro = document.getElementById("senhaCadastro").value;
+    const celularCadastro = document.getElementById("celularCadastro").value;
     const modalMensagem = document.getElementById("containerModalMensagem");
     const H3ModalMensagem = document.getElementById("mensangem");
 
-    const re = /\S+@\S+\.\S+/; // usado para validar o email
+    const validaEmail = /\S+@\S+\.\S+/; // usado para validar o email
+    const validaCelular = new RegExp("^\\([0-9]{2}\\)((3[0-9]{3}-[0-9]{5})|(9[0-9]{3}-[0-9]{4}))$"); // udado para validar o celular
 
-    modalMensagem.classList.add('mostrar');
     modalMensagem.addEventListener('click', (evento) => {
         if(evento.target.id == 'containerModalMensagem' || evento.target.className == 'fechaModal') {
             modalMensagem.classList.remove('mostrar');
         }
     });
 
-    if(!re.test(emailCadastro)) {
+    if(!nomeUsuario) {
 
+        H3ModalMensagem.innerHTML = "Nome é obrigatório!";
+        modalMensagem.classList.add('mostrar');
+
+    } else if(!emailCadastro) {
+
+        H3ModalMensagem.innerHTML = "Email é obrigatório!";
+        modalMensagem.classList.add('mostrar');
+
+    } else if(!validaEmail.test(emailCadastro)) {
+    
         H3ModalMensagem.innerHTML = "Digite um e-mail valido";
+        modalMensagem.classList.add('mostrar');
         
-    } else {
+    } else if(!senhaCadastro) {
 
-        if(!nomeUsuario || !emailCadastro || !senhaCadastro) {
-            H3ModalMensagem.innerHTML = "Verifique se todos os campos foram preenchidos!";
-        } else {
-            let objUsuario = { nomeUsuario : nomeUsuario, emailCadastro : emailCadastro, senhaCadastro : senhaCadastro };
-            let url = "http://localhost:4000/cadUser";
+        H3ModalMensagem.innerHTML = "Senha é obrigatório!";
+        modalMensagem.classList.add('mostrar');
+
+    } else if (!celularCadastro) {
+
+        H3ModalMensagem.innerHTML = "Celular é obrigatório!";
+        modalMensagem.classList.add('mostrar');
+        console.log(celularCadastro);
+
+    } else if(!validaCelular.test(celularCadastro)) {
+        H3ModalMensagem.innerHTML = "Digite um celular valido";
+        modalMensagem.classList.add('mostrar'); 
+        console.log(celularCadastro);
+    } else {
     
-            axios.post(url, objUsuario)
-            .then(response => {
-                if(response.data == "EMAIL JÁ CADASTRADO!") {
-                    H3ModalMensagem.innerHTML = response.data;
-                } else {
-                    H3ModalMensagem.innerHTML = "Usuário cadastrado com sucesso <br> Você será redirecionado ao tela de login";
-                    setTimeout( () => {
-                        window.location.href = "/login";
-                    }, 3000);
-                }
-    
-            })
-            .catch(erro => {
-                alert(erro);
-            })
-    
-        }
+        /* let objUsuario = { nomeUsuario : nomeUsuario, emailCadastro : emailCadastro, senhaCadastro : senhaCadastro };
+        let url = "http://localhost:4000/cadUser";
+
+        axios.post(url, objUsuario)
+        .then(response => {
+            if(response.data == "EMAIL JÁ CADASTRADO!") {
+                modalMensagem.classList.add('mostrar');
+                H3ModalMensagem.innerHTML = response.data;
+            } else {
+                modalMensagem.classList.add('mostrar');
+                H3ModalMensagem.innerHTML = "Usuário cadastrado com sucesso <br> Você será redirecionado ao tela de login";
+                setTimeout( () => {
+                    window.location.href = "/login";
+                }, 3000);
+            }
+
+        })
+        .catch(erro => {
+            alert(erro);
+        })*/
+
+        console.log(celularCadastro);
 
     }
 
+}
 
-
+function mascaraCelular(celular) {
+    if(celular.value.length == 0) {
+        celular.value = '(' + celular.value;
+    }
+ 
+    if(celular.value.length == 3) {
+        celular.value = celular.value + ')';
+    }
+ 
+    if(celular.value.length == 9) {
+        celular.value = celular.value + '-';
+    }
 }
